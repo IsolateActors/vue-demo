412b7345c48377c8fefdda9207b5849547438037
Zon <228657348@qq.com>
2020-10-22 11:52:16 +0800

    vant简单使用

---
+++ 47vue-vant/vantapp/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
+    <div>
+      <van-button
+        color="linear-gradient(to left, pink, skyblue)"
+        block
+        icon="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1603335650&di=f9738f22ace7bb5eeb6d9cf7ccd750f6&src=http://a0.att.hudong.com/70/91/01300000261284122542917592865.jpg"
+      >
+        渐变色按钮
+      </van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
